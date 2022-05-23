c9a66572873e00dbd986050c7714c52e5ea828d7
Bogdanna11 <78801541+Bogdanna11@users.noreply.github.com>
2022-05-23 03:23:20 -0700

    Update New.py

--- New.py
+++ New.py
@@ -1,1 +1,16 @@
-print("hello world")
+user_prompt = True
+my_username = "rusbogdana@yahoo.com"
+password = "pass1"
+while user_prompt:
+    username = input("What is your username")
+    if username.lower() == my_username:
+        print("well done the username is true")
+        user_password = input("please enter password")
+        if user_password == password:
+            print("Well done this is the correct password")
+            user_prompt = False
+            break
+        else:
+            print("Enter the password again")
+    else:
+        print("Please provide the correct username")
