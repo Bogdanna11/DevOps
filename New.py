5dde78b2cfe74702f0e3617e250befd53ca3b460
Bogdanna11 <78801541+Bogdanna11@users.noreply.github.com>
2022-05-23 09:18:23 -0700

    Update New.py

--- New.py
+++ New.py
@@ -1,9 +1,9 @@
 class Car:
-    def __init__(self, current_speed,breake,accelerate):
+    def __init__(self, current_speed):
         self.max_speed = 150
-        self.breake = breake
-        self.accelerate = accelerate
         self.current_speed = current_speed
+    def __repr__(self):
+         return f"the curent speed is {self.current_speed}"
     def get_this(self):
         return self.current_speed
     def set_accelerate(self, accelerate):
@@ -12,21 +12,17 @@ class Car:
             self.current_speed = self.current_speed + accelerate
         else:
             self.current_speed = self.current_speed + accelerate
+
     def set_brake(self, breake):
         if breake > 0:
-            breake = 0
-            self.current_break = breake
+           breake = 0
+           self.current_speed = breake
         else:
-            self.current_break = breake
+            self.current_speed = breake
 
-    def __repr__(self):
-        print(f"the car's speed is {self.current_speed}, if I accelerate it will be {self.set_accelerate},if I  breake it will be {self.set_brake}")
-
-
-
-new_car = Car(50,3,40)
-new_car.set_brake(3)
-new_car.set_accelerate(30)
+new_car = Car(50)
 print(new_car)
-
-
+new_car.set_accelerate(40)
+print(new_car)
+new_car.set_brake(3)
+print(new_car)
