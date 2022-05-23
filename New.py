31fee0ea56a0620ba33742eacd073b3a60e443b9
Bogdanna11 <78801541+Bogdanna11@users.noreply.github.com>
2022-05-23 08:04:07 -0700

    Update New.py

--- New.py
+++ New.py
@@ -1,17 +1,29 @@
-def multi_args_addition(*multiargs):
-    sum = 0
-    for arg in multiargs:
-        sum+=arg
-    return sum
-def substraction(x,y):
-    return x-y
-def multi_args_multiplication(*multiargs):
-     result = 1
-     for arg in multiargs:
-         result *=arg
-     return result
-def division(x,y):
-    return x/y
-print(multi_args_addition(3,4,5,6,4))
-print(multi_args_multiplication(1,2,2))
-print(substraction(5,2))
+class Car:
+    def __init__(self, current_speed):
+        self.max_speed = 150
+        self.current_speed = current_speed
+    def get_this(self):
+        return self.current_speed
+    def set_accelerate(self, accelerate):
+        if accelerate > self.max_speed:
+            accelerate = self.max_speed - self.current_speed
+            self.current_speed = self.current_speed + accelerate
+        else:
+            self.current_speed = self.current_speed + accelerate
+    def set_brake(self, breake):
+        if breake > 0:
+            breake = 0
+            self.current_break = breake
+        else:
+            self.current_break = breake
+
+    def __repr__(self):
+        print(f"the car's speed is {self.current_speed}, if I accelerate it will be {self.set_accelerate},if I  breake it will be {self.set_brake}")
+    
+
+
+new_car = Car(50)
+new_car.set_brake(3)
+new_car.set_accelerate(30)
+print(new_car)
+
