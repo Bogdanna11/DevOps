368e08b7f1249873552e8926590a7d7267050415
Bogdanna11 <78801541+Bogdanna11@users.noreply.github.com>
2022-05-24 02:03:20 -0700

    Create car.py

---
+++ car.py
@@ -0,0 +1,32 @@
+ def __init__(self, current_speed):
+        self.max_speed = 150
+        self.current_speed = current_speed
+    def __repr__(self):
+         return f"the curent speed is {self.current_speed}"
+    def get_this(self):
+        return self.current_speed
+    def set_accelerate(self, accelerate):
+        if accelerate < self.max_speed:
+            accelerate = self.max_speed - self.current_speed
+            self.current_speed = self.current_speed + accelerate
+            if self.current_speed > self.max_speed:
+               self.current_speed = self.max_speed
+        else:
+            self.accelerate = self.max_speed     
+            self.current_speed = self.accelerate
+    
+            
+
+    def set_brake(self, breake):
+        if breake > 0:
+           breake = 0
+           self.current_speed = breake
+        else:
+            self.current_speed = breake
+
+new_car = Car(50)
+print(new_car)
+new_car.set_accelerate(40)
+print(new_car)
+new_car.set_brake(3)
+print(new_car)
